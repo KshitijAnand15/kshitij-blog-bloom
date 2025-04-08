31538f8dbeeaee9f0bee34bb9145b9ca4e1d3599
Kshitijanand15 <kshitijanand648@gmail.com>
2025-04-08 17:40:20 +0530

    Updates

---
+++ assets/prodman-as-signal-DU87ZmPd.js
@@ -0,0 +1,24 @@
+const e=`---\r
+title: "Prodman as Signal"\r
+slug: "prodman-as-signal"\r
+date: "2024-04-27"\r
+description: "They are coming for our jobs"\r
+---\r
+\r
+A lot of times I have wondered about my decision to move to product management. Questioned even.\r
+\r
+It kind of resonated with the job I had before, and it is supposed to be a high-paying job. I think that was it. There must be more. But since I got into Prodman, I felt my energy went down significantly. It made me feel I was doing all those prodman exercises just to get a job. A lot of us are in this boat.\r
+\r
+Manas Saloi’s blog ‘Risk to PM Jobs’ just jolted me out of my comfort zone as a PM.\r
+\r
+Was it just a status-seeking job for me? I know I will thrive in a generalist role, but did I go for the most prestigious one? Founder’s office also would have been great. Right?\r
+\r
+I don’t remember the last time I deeply thought about a problem statement before reading the blog. I used to dissect products in terms of their onboarding, upselling, and any interesting UX that caught my eye. But I stopped doing it the moment I got into Prodman.\r
+\r
+And I don’t want that to be the case. I remember the enthusiasm. The excitement of getting to know what PMs of X have done and wondering why. I remember discussing anything peculiar that caught my eye with my friends. Now, I think I’ve started taking the PM role for granted. So has a lot of people.\r
+\r
+Just listening to Lenny won’t make the cut. The world is changing very fast, and PMs are the guiding beacon that shows light for ICs like devs and designers to create something.\r
+\r
+No tool will make us PMs successful other than sheer curiosity in the craft of product management. It took me a blog to jolt me out of a slumber.\r
+\r
+Maybe this will end your slumber as well.`;export{e as default};
