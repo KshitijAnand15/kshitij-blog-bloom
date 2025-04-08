cfe5989d290ce45e44231ecb85ba136c062c6b4a
Kshitijanand15 <kshitijanand648@gmail.com>
2025-04-08 12:23:54 +0530

    Updates

---
+++ assets/How I built this-Bjsnysw0.js
@@ -0,0 +1,12 @@
+const t=`---\r
+title: "How I Built This Blog"\r
+slug: "how-i-built-this"\r
+date: "2025-04-07"\r
+description: "A behind-the-scenes look at building this blog from scratch."\r
+---\r
+\r
+Welcome to my first post!  \r
+Here’s how I set up my blog using Vite, React, and GitHub Pages...\r
+\r
+It was surprisingly fun 🔥\r
+`;export{t as default};
