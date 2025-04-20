d22b3a53442d9bcef8b385e0cd2d5b0015d30196
Kshitijanand15 <kshitijanand648@gmail.com>
2025-04-21 00:16:31 +0530

    Updates

---
+++ assets/adolescence-Dxm5jT6P.js
@@ -0,0 +1,48 @@
+const e=`---\r
+title: "Adolescence"\r
+slug: "adolescence"\r
+date: "2025-04-20"\r
+description: "Thoughts on the show"\r
+---\r
+\r
+Most of the things I saw in Adolescence are not new. Except for the murder part , which was horrible, there was nothing shocking.\r
+\r
+And I can tell you this as someone who turned teen when the internet just picked up.\r
+\r
+People say the internet has done this and that, but if you watch episode 3 closely, you'll find such people even before the internet era.\r
+The idea of a 'gettable'woman who is down bad or going through a breakup and hence is ‘easy’ is not something that originated from the internet. I’ve been given this advice first when I was 14. In person. It’s not new. And in terms of internet years, I am a dinosaur.\r
+Rene Girard introduced the idea of Memesis (everyone should read it) and it says almost all human beings’ needs and desires do not come from within but from the outside world.\r
+\r
+People want what others have. You start seeing such behaviors in babies very early on. \r
+\r
+If a lot of people desiring the same thing, you’d want that too. Having a pricey car gives them status, at least in Indian society. Most people want cars because they want to be seen with the car.\r
+\r
+Women are status symbols. Most men want to be seen with certain women. Most men flock to the same 10% of women not because they like them, love them, or want to know them. It is because they want to be seen with them. \r
+\r
+In the case of Jamie, he said Katie is not his type. But she’s ‘gettable’ and he wants to be seen with a girl so that bullies respect him.\r
+\r
+Now the second aspect is to consider the opposite gender as equal. Here, the Internet comes into the picture but even before the Internet days, this was a problem.\r
+A lot of guys do not consider women to be their equals. Their entire idea is to dominate them because they have seen them being dominated. \r
+\r
+A lot of kids try to dominate their mothers and sisters. A lot of kids saw females getting dominated at home. \r
+\r
+ The way Jamie was trying with the therapist. Putting her down, shouting at her because I think he thinks doing this gives him the upper edge.\r
+\r
+“Are you scared of a 13 yo?” He says which kind of establishes my hypothesis that he just wanted to dominate her and that he’s in charge. \r
+\r
+But what is interesting is rejection. When you get rejected by someone who thinks is inferior, I can just imagine the pain and hurt that would bring. Episode 3 ending is that pain and hurt.\r
+\r
+Their fragile male ego got hurt because of a girl he didn’t consider as his type because of her different body type but she was gettable and lesser now that some photos leaked and now she rejected him. Oooof. That would’ve stung.\r
+\r
+Now, the internet comes into the picture because there are places where the idea of women being inferior is propagated very aggressively. The Internet is a catalyst here. All these incel, red pill black pill stuff is related to that. Shame around not having a girlfriend or worse getting rejected by a woman. And worse, conspiracy theories around this. \r
+\r
+Oh, you got rejected by an inferior gender who has been given way too much power? The world is unfair and rigged and we need to fight back and take it back from women and men they are attracted to.\r
+\r
+There are internet subcultures that support what I wrote when I was researching for this blog. Horrifying is not even the word.\r
+\r
+But nothing I saw was shocking. If you’re someone who can catch cues, you’ll see all these things existed long before the internet.\r
+The Internet gave them a place to flourish and improved their distribution and now they’re reaching every kid with a phone in his hand. \r
+\r
+Scary. Very very scary.\r
+I may be generalizing a bit here and there but this is my read of the show.\r
+`;export{e as default};
